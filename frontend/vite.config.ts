afad951a63c1a054b654124d1e80735ae7b9192c
shreyasexec <shreyas.exec@gmail.com>
2025-12-03 21:22:25 +0530

    dsjkds

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
+    https: {
+      key: fs.readFileSync('/etc/ssl/private/localhost-key.pem'),
+      cert: fs.readFileSync('/etc/ssl/certs/localhost.pem'),
+    },
   },
 })
